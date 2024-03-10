bcaf947979f38240706cfd2f80e5c2f925a63a9d
Anandhugithub1 <122972305+Anandhugithub1@users.noreply.github.com>
2024-03-10 22:29:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // imports
 const express =require('express')
+const cors = require('cors');
+
 const connectdb =require('./db/connect')
 require('dotenv').config();
 const authRouter =require('./routes/auth')
@@ -7,9 +9,13 @@ const donerRouter =require('./routes/donor')
 
 
 const app =express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors({
+    origin: '*', // Allow requests from any origin
+    methods: ['GET', 'POST'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+}));
 
-  
 app.use(authRouter)
 app.use(donerRouter);
 
