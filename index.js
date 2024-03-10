02f8666c89956249dc58c6928c56c47eb3d89f82
Anandhugithub1 <122972305+Anandhugithub1@users.noreply.github.com>
2024-03-10 22:00:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const app =express();
 app.use(express.json())
 app.use(cors({
-    origin: 'https://donorapi-production.up.railway.app',
+    origin: '*',
     methods: ['GET', 'POST'], // Allow specific HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
   }));
