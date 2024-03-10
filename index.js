e5d7cb5f8a2589da4748dbd41ef84e60f8326444
unknown <anantuashokan333@gmail.com>
2024-03-10 17:50:22 +0530

    index file changed

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const connectdb =require('./db/connect')
 require('dotenv').config();
 const authRouter =require('./routes/auth')
 const donerRouter =require('./routes/donor')
+const cors = require('cors');
+
 
 const app =express();
 app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:52099',
+    methods: ['GET', 'POST'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  }));
+  
 app.use(authRouter)
 app.use(donerRouter);
 
