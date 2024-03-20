13a27e220e031506a6fc7c93c2045af2bb8b778b
Anandhugithub1 <anantuashokan333@gmail.com>
2024-03-20 22:56:59 +0530

    changed uri

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const uri = 'mongodb+srv://anandhu2a2256:anandhu123@cluster0.vuqaskm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
 
 const connect = async () => {
     try {
         console.log('Connecting to the database...');
-        const uri = process.env.uri;
         await mongoose.connect(uri);
         console.log('Connected to the database successfully!');
     } catch (error) {
