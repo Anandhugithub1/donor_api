01a7c79653a7440b4093d37c89abc3eab6f23829
Anandhugithub1 <anantuashokan333@gmail.com>
2024-03-20 19:26:02 +0530

    removed senstitive data

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connect = async () => {
     try {
         console.log('Connecting to the database...');
-        const uri = process.env.uri;
+        const uri = 'mongodb+srv://anandhu2a2256:anandhu123@cluster0.vuqaskm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
         await mongoose.connect(uri);
         console.log('Connected to the database successfully!');
     } catch (error) {
